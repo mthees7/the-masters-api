c283004f449dcfcff5d13913a0ecd8954885c91a
Matt Thees <mthees@Matts-MacBook-Pro.local>
2019-04-14 13:56:52 -0500

    updating indexes to reflect final results

--- parse_players.sh
+++ parse_players.sh
@@ -14,10 +14,10 @@ pup_path='.competitors tbody tr.Table2__tr json{}'
     "$bin_dir/jq" '.[1:]' | \
     "$bin_dir/jq" '[.[] | {
         pos: .children[0].text,
-        player: .children[2].children[1].text,
+        player: .children[1].children[1].text,
         country_flag_image: .children[2].children[0].src,
-        link: .children[2].children[1].href,
-        to_par: .children[3].text,
+        link: .children[1].children[1].href,
+        to_par: .children[2].text,
         today: .children[4].text,
         thru: .children[5].text,
         r1: .children[6].text,
